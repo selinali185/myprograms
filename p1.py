79388eaf7195e02b8403cd3168f2121c2d1e20f0
selinali185 <selina.li185@gmail.com>
2025-09-15 10:55:30 -0400

    lol

---
+++ p1.py
@@ -0,0 +1,11 @@
+# hai
+# My first program
+# Created on 09/15/2025
+# Purpose: to learn python
+# Editor: Selina Li
+
+#intructions: input name and age, output 1 line greeting and acknowledge their name and age
+
+name = input("Hey there, can I have your name? ")
+age = input("How old are you? ")
+print(f"Your name is {name} and you are {age} years old.")
