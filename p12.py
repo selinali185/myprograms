3395f1275790772ac74e95022d523b6e0db777db
selinali185 <selina.li185@gmail.com>
2025-10-17 10:44:40 -0400

    code cleaning question

--- p12.py
+++ p12.py
@@ -1,9 +1,47 @@
 # code cleaning crew to find only upper and postiive int
 
-unclean = input("enter product code: ")
-lowercase = ""
-uppercase = ""
-non_letters = ""
-for item in unclean: 
-    if item.isalpha() and item.islower(): 
+def cleaner(text):
+    # text looks like cG23mH-9s
+    # we want GH14
+    
+    uppercase = ""
+    positives = ""
+    negatives = ""
+    total_sum = 0
+    for item in text:
+        if item.isalpha() and item.isupper():
+            uppercase += item 
+            if len(positives) > 0:
+                total_sum += int(positives)
+                positives = ""
+            if len(negatives) > 0:
+                total_sum += int(negatives)
+                negatives = ""
+
+        elif item == "-":
+            if len(negatives) > 0:
+                total_sum += int(negatives)
+                negatives = "-"
+            else: 
+                negatives = "-"
+        elif item.isdigit():
+            if len(negatives) > 0:
+                negatives += item
+            else: 
+                positives += item
+
+            # end of loop
+
         
+        #print(item)
+
+    if len(positives) > 0:
+        total_sum += int(positives)
+    if len(negatives) > 0:
+        total_sum += int(negatives)
+    
+    product_code = uppercase + str(total_sum)
+    return product_code
+
+    print(cleaner("cG23mH-9s"))
+    
