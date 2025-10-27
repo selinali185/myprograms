e7aa4f538c0c75bbd215a0d24047032d28dab0a6
selinali185 <selina.li185@gmail.com>
2025-10-27 10:55:22 -0400

    insertion sort

--- p15.py
+++ p15.py
@@ -1,3 +1,16 @@
 # insertion sort 
 
 def insertion(a_list):
+    i = 1
+    while i < len(a_list):
+        j = i 
+        while j > 0:
+            if a_list[j-1] > a_list[j]:
+                a_list[j-1], a_list[j] = a_list[j], a_list[j-1]
+            else: 
+                break # gets out of while loop 
+
+            j -= 1 # j = j - 1
+
+        i += 1
+
